55ada7ffd64bb059753ed604b8d03d8a850bca5b
TBHNhien <118433904+TBHNhien@users.noreply.github.com>
2024-06-19 01:03:34 +0700

    BAI17

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import "./App.css";
+import Toggle from "./components/state/Toggle";
 import YoutubeList from "./components/youtube/YoutubeList";
 function App() {
   const name = "Evondev";
   return (
     <div>
-      <YoutubeList>
-        {name}
-        {/* <h2>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-          laborum ea consectetur fugit eum tempore dolores nulla! Repellendus,
-          nisi voluptas sed suscipit recusandae, nulla excepturi necessitatibus
-          eaque alias consequuntur incidunt.
-        </h2> */}
-        {/*children props : html, text, variable */}
-      </YoutubeList>
+      <Toggle></Toggle>
     </div>
   );
 }
--- src/components/state/Toggle.js
+++ src/components/state/Toggle.js
@@ -11,8 +11,18 @@ import React, { useState } from "react";
 // }
 
 function Toggle() {
-  const array = useState(false);
-  console.log(array);
+  // const array = useState(false);
+  // console.log(array); //[false, function] -> function để update state
+  // console.log(array[0]);
+  // console.log(array[1]);
+  // const arr = [1, 2];
+  // console.log(arr[0], arr[1]); // destructor?
+  // const [a, b] = [1, 2];
+  // console.log(a, b);
+
+  const [on, setOn] = useState(false);
+  console.log(on, setOn);
+
   return <div className="toggle"></div>;
 }
 export default Toggle;
